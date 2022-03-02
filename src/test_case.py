740f6d34d78d122a455272a98dbee52137b8eea5
vschweitzer <valentin.schweitzer98@gmail.com>
2022-03-03 00:30:54 +0100

    Add largest-slice method in addition to some constructor checks

--- src/test_case.py
+++ src/test_case.py
@@ -2,10 +2,11 @@
 Handles the representation of patients/test cases
 """
 
-from typing import Any, Callable
+from typing import Any, Callable, Optional
 import json
 import hashlib
 import nibabel
+import numpy as np
 
 import storable
 
@@ -22,8 +23,8 @@ class TestCaseBase(storable.Storable):
     id: str
 
     # not stored
-    anno_object: object
-    scan_object: object
+    anno_object: Any
+    scan_object: Any
 
     def __init__(self, scan_path: str, anno_path: str, category: int) -> None:
         super().__init__()
@@ -33,6 +34,18 @@ class TestCaseBase(storable.Storable):
         self.id = self.get_id()
         self.scan_object = nibabel.load(self.scan_path)
         self.anno_object = nibabel.load(self.anno_path)
+        if (
+            self.scan_object.header.get_data_shape()
+            != self.anno_object.header.get_data_shape()
+        ):
+            raise ValueError(
+                f"Data dimensions do not match: {self.scan_object.header.get_data_shape()} != {self.anno_object.header.get_data_shape()}"
+            )
+
+        if self.scan_object.header.get_zooms() != self.anno_object.header.get_zooms():
+            raise ValueError(
+                f"Voxel dimensions do not match: {self.scan_object.header.get_zooms()} != {self.anno_object.header.get_zooms()}"
+            )
 
     def get_id(
         self, hash_function: Callable = hashlib.sha3_256, buffer_size: int = 4096
@@ -99,7 +112,38 @@ class TestCaseBase(storable.Storable):
         return cls(scan_path, anno_path, category)
 
     def get_index_of_largest_slice(self):
-        pass
+        """
+        Returns the largest slice of a scans' annotation.
+        A "slice" has n-1 dimensions, where n is the number of dimensions of
+        the original image.
+
+        This will produce unexpected results for images with != 3 dimensions.
+        """
+
+        def get_slice_index(axis: int, index: int, dimensions: int = 3):
+            slice_list: list = [slice(None) for _ in range(dimensions)]
+            slice_list[axis] = slice(index, index + 1, None)
+            return tuple(slice_list)
+
+        anno_shape: list = self.anno_object.header.get_data_shape()
+        anno_voxel_dimensions: list = self.anno_object.header.get_zooms()
+        maximum_slice_area: float = 0
+        maximum_slice_area_index: Optional[list] = None
+        for dimension in range(len(self.anno_object.header.get_data_shape())):
+            slice_voxel_dimensions: list = (
+                anno_voxel_dimensions[:dimension]
+                + anno_voxel_dimensions[dimension + 1 :]
+            )
+            slice_voxel_area: float = np.prod(slice_voxel_dimensions)
+            for slice_number in range(anno_shape[dimension]):
+                slice_index = get_slice_index(dimension, slice_number, len(anno_shape))
+                voxel_slice: list = self.anno_object.get_fdata()[slice_index]
+                nonzero_voxels: int = np.count_nonzero(voxel_slice)
+                slice_area: float = nonzero_voxels * slice_voxel_area
+                if slice_area > maximum_slice_area:
+                    maximum_slice_area = slice_area
+                    maximum_slice_area_index = slice_index
+        return maximum_slice_area_index
 
 
 class TestCase(TestCaseBase):
@@ -119,11 +163,11 @@ class TestCase(TestCaseBase):
 
 
 if __name__ == "__main__":
-    test_scan_path: str = "../Dataset_V2/MR100.nii.gz"
-    test_save_path: str = "./100_save.txt"
+    test_scan_path: str = "../Dataset_V2/MR81.nii.gz"
+    test_save_path: str = "./81_save.txt"
     tc: TestCase = TestCase.from_scan_path(scan_path=test_scan_path, category=0)
     tc.to_file(test_save_path)
     tc2: TestCase = TestCase.from_file(test_save_path)
 
-    print(tc)
-    print(tc2)
+    print(tc.get_index_of_largest_slice())
+    print(tc2.get_index_of_largest_slice())
