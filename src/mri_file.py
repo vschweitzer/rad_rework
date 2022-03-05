57c154abc9e17bfa5ac3f0e92b891a9e312dac7c
vschweitzer <valentin.schweitzer98@gmail.com>
2022-03-06 00:27:59 +0100

    Split mri files into their own file and classes, add alternative constructors

---
+++ src/mri_file.py
@@ -0,0 +1,157 @@
+"""
+Handles representation of MRI (scan/annotation) files in the form of NIfTI-1.
+"""
+
+
+from typing import Any, Callable, Optional
+import json
+import hashlib
+import nibabel
+import numpy as np
+import os
+
+import storable
+
+
+class MRIFile(storable.Storable):
+    """
+    NIfTI-1 MRI file.
+    """
+
+    path: str
+    image: nibabel.Nifti1Image
+
+    def __init__(self, path) -> None:
+        self.path = path
+        self.image = nibabel.load(self.path)
+
+    def get_dict_representation(self):
+        dict_representation: dict = super().get_dict_representation()
+        dict_representation["path"] = self.path
+        dict_representation["id"] = self.get_id()
+        return dict_representation
+
+    def to_file(self, file_path: str):
+        with open(file_path, "w") as output_file:
+            json.dump(self.get_dict_representation(), output_file)
+
+    @classmethod
+    def from_file(cls, file_path: str):
+        with open(file_path, "r") as input_file:
+            dict_representation: dict = json.load(input_file)
+            return MRIFile.from_dict(dict_representation)
+
+    @classmethod
+    def from_dict(cls, dict_representation: dict):
+        new_instance: MRIFile = cls(dict_representation["path"])
+        if new_instance.get_id() != dict_representation["id"]:
+            raise ValueError(
+                "ID of loaded image does not match saved ID. The image file has been changed."
+            )
+        return new_instance
+
+    def get_id(
+        self, hash_function: Callable = hashlib.sha3_256, buffer_size: int = 4096
+    ):
+        """
+        Consists of a file hash.
+        """
+        # https://stackoverflow.com/questions/22058048/hashing-a-file-in-python
+
+        digestor: Any = hash_function()
+        with open(self.path, "rb") as input_file:
+            while True:
+                data: bytes = input_file.read(buffer_size)
+                if not data:
+                    break
+                digestor.update(data)
+        return digestor.hexdigest()
+
+
+class ScanFile(MRIFile):
+    def __init__(self, path) -> None:
+        super().__init__(path)
+
+
+class AnnoFile(MRIFile):
+    def __init__(self, path) -> None:
+        super().__init__(path)
+
+    @classmethod
+    def to_largest_slice(
+        cls, path: str, anno_object: nibabel.Nifti1Image, overwrite: bool = False
+    ):
+        """
+        Creates new annotation from given by keeping only the largest slice.
+        The new annotation is saved in path.
+        """
+        if not overwrite:
+            if os.path.exists(path):
+                raise FileExistsError("File in path already exists.")
+        largest_slice_anno: nibabel.Nifti1Image = AnnoFile._largest_slice_anno(
+            anno_object
+        )
+        nibabel.save(largest_slice_anno, path)
+        return AnnoFile(path)
+
+    @staticmethod
+    def _get_largest_slice_index(anno_object: nibabel.Nifti1Image):
+        """
+        Returns the largest slice of a scans' annotation.
+        A "slice" has n-1 dimensions, where n is the number of dimensions of
+        the original image.
+
+        This will produce unexpected results for images with != 3 dimensions.
+        """
+
+        def _get_slice_index(axis: int, index: int, dimensions: int = 3):
+            slice_list: list = [slice(None) for _ in range(dimensions)]
+            slice_list[axis] = slice(index, index + 1, None)
+            return tuple(slice_list)
+
+        anno_shape: list = anno_object.header.get_data_shape()
+        anno_voxel_dimensions: list = anno_object.header.get_zooms()
+        maximum_slice_area: float = 0
+        maximum_slice_area_index: Optional[list] = None
+        for dimension in range(len(anno_object.header.get_data_shape())):
+            slice_voxel_dimensions: list = (
+                anno_voxel_dimensions[:dimension]
+                + anno_voxel_dimensions[dimension + 1 :]
+            )
+            slice_voxel_area: float = np.prod(slice_voxel_dimensions)
+            for slice_number in range(anno_shape[dimension]):
+                slice_index = _get_slice_index(dimension, slice_number, len(anno_shape))
+                voxel_slice: list = anno_object.get_fdata()[slice_index]
+                nonzero_voxels: int = np.count_nonzero(voxel_slice)
+                slice_area: float = nonzero_voxels * slice_voxel_area
+                if slice_area > maximum_slice_area:
+                    maximum_slice_area = slice_area
+                    maximum_slice_area_index = slice_index
+        return maximum_slice_area_index
+
+    @staticmethod
+    def _largest_slice_anno(anno_object: nibabel.Nifti1Image):
+        # TODO: Split scan and anno into their own classes, use second anno
+        # object to cache this
+
+        slice_anno_data = np.zeros(anno_object.header.get_data_shape())
+        largest_slice_index: list = AnnoFile._get_largest_slice_index(anno_object)
+        slice_anno_data[largest_slice_index] = anno_object[largest_slice_index]
+        return nibabel.Nifti1Image(
+            slice_anno_data, anno_object.affine, anno_object.header
+        )
+
+    @classmethod
+    def from_scan_path(
+        cls,
+        scan_path: str,
+        anno_suffix: str = "A",
+        file_ending: str = ".nii.gz",
+    ):
+        no_ending: str
+        if scan_path.endswith(file_ending):
+            no_ending = scan_path[: -len(file_ending)]
+        else:
+            no_ending = scan_path
+        anno_path: str = no_ending + anno_suffix + file_ending
+        return cls(anno_path)
--- src/test_case.py
+++ src/test_case.py
@@ -7,18 +7,10 @@ import json
 import hashlib
 import nibabel
 import numpy as np
+import os
 
 import storable
-
-
-class ScanFile:
-    def __init__(self, path: str) -> None:
-        pass
-
-
-class AnnoFile:
-    def __init__(self, path: str) -> None:
-        pass
+import mri_file
 
 
 class TestCaseBase(storable.Storable):
@@ -27,8 +19,9 @@ class TestCaseBase(storable.Storable):
     """
 
     # stored
-    scan_path: str
-    anno_path: str
+    scan_file: mri_file.ScanFile
+    anno_file: mri_file.AnnoFile
+
     category: int
     id: str
 
@@ -36,25 +29,29 @@ class TestCaseBase(storable.Storable):
     anno_object: nibabel.Nifti1Image
     scan_object: nibabel.Nifti1Image
 
-    def __init__(self, scan_path: str, anno_path: str, category: int) -> None:
+    def __init__(
+        self, scan_file: mri_file.ScanFile, anno_file: mri_file.AnnoFile, category: int
+    ) -> None:
         super().__init__()
-        self.scan_path = scan_path
-        self.anno_path = anno_path
+        self.scan_file = scan_file
+        self.anno_file = anno_file
         self.category = category
         self.id = self.get_id()
-        self.scan_object = nibabel.load(self.scan_path)
-        self.anno_object = nibabel.load(self.anno_path)
+
         if (
-            self.scan_object.header.get_data_shape()
-            != self.anno_object.header.get_data_shape()
+            self.scan_file.image.header.get_data_shape()
+            != self.anno_file.image.header.get_data_shape()
         ):
             raise ValueError(
-                f"Data dimensions do not match: {self.scan_object.header.get_data_shape()} != {self.anno_object.header.get_data_shape()}"
+                f"Data dimensions do not match: {self.scan_file.image.header.get_data_shape()} != {self.anno_file.image.header.get_data_shape()}"
             )
 
-        if self.scan_object.header.get_zooms() != self.anno_object.header.get_zooms():
+        if (
+            self.scan_file.image.header.get_zooms()
+            != self.anno_file.image.header.get_zooms()
+        ):
             raise ValueError(
-                f"Voxel dimensions do not match: {self.scan_object.header.get_zooms()} != {self.anno_object.header.get_zooms()}"
+                f"Voxel dimensions do not match: {self.scan_file.image.header.get_zooms()} != {self.anno_file.image.header.get_zooms()}"
             )
 
     def get_id(
@@ -65,23 +62,18 @@ class TestCaseBase(storable.Storable):
         """
         # https://stackoverflow.com/questions/22058048/hashing-a-file-in-python
 
-        hashes = []
-        for input_path in [self.scan_path, self.anno_path]:
-            digestor: Any = hash_function()
-            with open(input_path, "rb") as input_file:
-                while True:
-                    data: bytes = input_file.read(buffer_size)
-                    if not data:
-                        break
-                    digestor.update(data)
-                hashes.append(digestor.hexdigest())
-
-        return "_".join(hashes)
+        return (
+            self.scan_file.get_id(hash_function=hash_function, buffer_size=buffer_size)
+            + "_"
+            + self.anno_file.get_id(
+                hash_function=hash_function, buffer_size=buffer_size
+            )
+        )
 
     def get_dict_representation(self):
         dict_representation: dict = super().get_dict_representation()
-        dict_representation["scan_path"] = self.scan_path
-        dict_representation["anno_path"] = self.anno_path
+        dict_representation["scan_file"] = self.scan_file.get_dict_representation()
+        dict_representation["anno_file"] = self.anno_file.get_dict_representation()
         dict_representation["category"] = self.category
         dict_representation["id"] = self.id
         return dict_representation
@@ -99,9 +91,16 @@ class TestCaseBase(storable.Storable):
         with open(file_path, "r") as input_file:
             dict_representation = json.load(input_file)
 
+        scan_file: mri_file.ScanFile = mri_file.ScanFile.from_dict(
+            dict_representation["scan_file"]
+        )
+        anno_file: mri_file.AnnoFile = mri_file.AnnoFile.from_dict(
+            dict_representation["anno_file"]
+        )
+
         return cls(
-            dict_representation["scan_path"],
-            dict_representation["anno_path"],
+            scan_file,
+            anno_file,
             dict_representation["category"],
         )
 
@@ -113,13 +112,11 @@ class TestCaseBase(storable.Storable):
         anno_suffix: str = "A",
         file_ending: str = ".nii.gz",
     ):
-        no_ending: str
-        if scan_path.endswith(file_ending):
-            no_ending = scan_path[: -len(file_ending)]
-        else:
-            no_ending = scan_path
-        anno_path: str = no_ending + anno_suffix + file_ending
-        return cls(scan_path, anno_path, category)
+        scan_file: mri_file.ScanFile = mri_file.ScanFile(scan_path)
+        anno_file: mri_file.AnnoFile = mri_file.AnnoFile.from_scan_path(
+            scan_path, anno_suffix=anno_suffix, file_ending=file_ending
+        )
+        return cls(scan_file, anno_file, category)
 
 
 class TestCase(TestCaseBase):
@@ -128,55 +125,16 @@ class TestCase(TestCaseBase):
     functions.
     """
 
-    def __init__(self, scan_path: str, anno_path: str, category: int) -> None:
-        super().__init__(scan_path, anno_path, category)
+    def __init__(
+        self, scan_file: mri_file.ScanFile, anno_file: mri_file.AnnoFile, category: int
+    ) -> None:
+        super().__init__(scan_file, anno_file, category)
         self.cached: dict = {}
 
     def get_dict_representation(self):
         dict_representation: dict = super().get_dict_representation()
         dict_representation["cached"] = self.cached
         return dict_representation
-
-    def get_largest_slice_index(self):
-        """
-        Returns the largest slice of a scans' annotation.
-        A "slice" has n-1 dimensions, where n is the number of dimensions of
-        the original image.
-
-        This will produce unexpected results for images with != 3 dimensions.
-        """
-
-        def get_slice_index(axis: int, index: int, dimensions: int = 3):
-            slice_list: list = [slice(None) for _ in range(dimensions)]
-            slice_list[axis] = slice(index, index + 1, None)
-            return tuple(slice_list)
-
-        anno_shape: list = self.anno_object.header.get_data_shape()
-        anno_voxel_dimensions: list = self.anno_object.header.get_zooms()
-        maximum_slice_area: float = 0
-        maximum_slice_area_index: Optional[list] = None
-        for dimension in range(len(self.anno_object.header.get_data_shape())):
-            slice_voxel_dimensions: list = (
-                anno_voxel_dimensions[:dimension]
-                + anno_voxel_dimensions[dimension + 1 :]
-            )
-            slice_voxel_area: float = np.prod(slice_voxel_dimensions)
-            for slice_number in range(anno_shape[dimension]):
-                slice_index = get_slice_index(dimension, slice_number, len(anno_shape))
-                voxel_slice: list = self.anno_object.get_fdata()[slice_index]
-                nonzero_voxels: int = np.count_nonzero(voxel_slice)
-                slice_area: float = nonzero_voxels * slice_voxel_area
-                if slice_area > maximum_slice_area:
-                    maximum_slice_area = slice_area
-                    maximum_slice_area_index = slice_index
-        return maximum_slice_area_index
-
-    def largest_slice_anno(self):
-        # TODO: Split scan and anno into their own classes, use second anno
-        # object to cache this
-
-        slice_anno_data: np.array = np.zeros(self.anno_object.header.get_data_shape())
-        largest_slice_index: list = self.get_largest_slice_index()
 
 
 if __name__ == "__main__":
@@ -186,5 +144,5 @@ if __name__ == "__main__":
     tc.to_file(test_save_path)
     tc2: TestCase = TestCase.from_file(test_save_path)
 
-    print(tc.get_largest_slice_index())
-    print(tc2.get_largest_slice_index())
+    print(tc)
+    print(tc2)
