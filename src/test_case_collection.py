db51091dd2e5815dfdfac86e3bdd5e3fba9d6c38
vschweitzer <valentin.schweitzer98@gmail.com>
2022-03-20 22:36:20 +0100

    Add basic TestCase sampling method

--- src/test_case_collection.py
+++ src/test_case_collection.py
@@ -1,13 +1,10 @@
 """
 Defines TestCaseCollection, which offers loading and selection methods.
 """
-
-
-import json
+import random
 import os
 import csv
-from typing import List, Optional
-
+from typing import List, Optional, Dict, Union
 
 import storable
 import test_case
@@ -19,21 +16,28 @@ class TestCaseCollection(storable.Storable):
     methods.
     """
 
-    test_cases: List[test_case.TestCase]
+    test_cases: Dict[str, test_case.TestCase]
     annotation_mode: str
+    random_seed: int
+    _random_provider: random.Random
 
-    def __init__(self, test_cases: List[test_case.TestCase]) -> None:
+    def __init__(
+        self, test_cases: List[test_case.TestCase], random_seed: int = 0
+    ) -> None:
         """
         test_cases is a list of TestCases.
         """
         super().__init__()
-        self.test_cases = test_cases
+        self.test_cases = dict((tc.get_id(), tc) for tc in test_cases)
+        self.random_seed = random_seed
+        self._random_provider = random.Random(self.random_seed)
 
     def get_dict_representation(self):
         dict_representation: dict = super().get_dict_representation()
         dict_representation["test_cases"] = [
-            tc.get_dict_representation() for tc in self.test_cases
+            tc.get_dict_representation() for tc in self.test_cases.values()
         ]
+        dict_representation["random_seed"] = self.random_seed
         return dict_representation
 
     def convert_annotations(
@@ -66,7 +70,7 @@ class TestCaseCollection(storable.Storable):
 
             for tc in self.test_cases:
                 test_case.TestCase.to_2D(
-                    tc,
+                    self.test_cases[tc],
                     None,
                     overwrite=overwrite,
                     use_existing=use_existing,
@@ -84,6 +88,7 @@ class TestCaseCollection(storable.Storable):
         csv_path: str,
         file_ending: str = ".nii.gz",
         skip_invalid: bool = True,
+        random_seed: int = 0,
     ):
         """
         Load TestCaseCollection from csv file.
@@ -121,7 +126,55 @@ class TestCaseCollection(storable.Storable):
 
                 test_cases.append(tc)
 
-        return cls(test_cases)
+        return cls(test_cases, random_seed)
+
+    def get_equal_sample(
+        self, count: Union[int, float], fractional: bool = True, metric: str = "pcr"
+    ) -> list:
+        """
+        Return a sample of IDs of TestCases taken equally from each category.
+
+        count: Either:
+         - the absolute number of cases per category as an integer, if
+           "fractional" is False.
+         - the share of samples to take from the smallest category as a float,
+           if "fractional" is True.
+        metric: Determines which metric to split the categories along (either
+                "pcr" or "nar").
+        """
+        if metric not in ["nar", "pcr"]:
+            raise ValueError(f'Metric "{metric}" is not recognized.')
+        if fractional and (count < 0.0 or count > 1.0):
+            raise ValueError(f"{count} out of range for fractional value.")
+
+        chosen: list = []
+        categories: dict = {}
+        for key in self.test_cases:
+            tc: test_case.TestCase = self.test_cases[key]
+            tc_category: Optional[Union[int, bool]] = (
+                tc.nar if metric == "nar" else tc.pcr
+            )
+            if tc_category is None:
+                continue
+            if tc_category not in categories:
+                categories[tc_category] = []
+            categories[tc_category].append(key)
+
+        min_available_per_category: int = min([len(categories[c]) for c in categories])
+        to_choose: int
+        if fractional:
+            to_choose = round(count * min_available_per_category)
+        else:
+            if not isinstance(count, int):
+                raise TypeError("Expecting int for non-fractional count.")
+            to_choose = count
+        if to_choose > min_available_per_category:
+            raise ValueError("Trying to select more values than available.")
+
+        for c in categories:
+            chosen.append(self._random_provider.sample(categories[c], to_choose))
+
+        return chosen
 
 
 if __name__ == "__main__":
@@ -134,3 +187,5 @@ if __name__ == "__main__":
     pp.pprint(tcc.get_dict_representation())
     tcc.convert_annotations(conversion_options={"along_axes": [2]})
     pp.pprint(tcc.get_dict_representation())
+    print(tcc.get_equal_sample(1, fractional=False, metric="pcr"))
+    print(tcc.get_equal_sample(1, fractional=False, metric="nar"))
