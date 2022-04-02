36df4c6cd18f6a7874128307c2a3fe16d47ffadc
vschweitzer <valentin.schweitzer98@gmail.com>
2022-04-02 19:36:18 +0200

    Add more unified saving/loading functions

--- src/storable.py
+++ src/storable.py
@@ -17,12 +17,24 @@ class Storable:
         """
         Save class instance to file
         """
+        with open(file_path, "w") as output_file:
+            json.dump(self.get_dict_representation(), output_file)
+
+    @classmethod
+    def from_dict(cls, dict_representation: dict):
+        """
+        Load class from dictionary representation
+        """
+        return cls()
 
     @classmethod
     def from_file(cls, file_path: str):
         """
         Load class instance from file
         """
+        with open(file_path, "r") as input_file:
+            dict_representation = json.load(input_file)
+            return cls.from_dict(dict_representation)
 
     def __str__(self) -> str:
         return json.dumps(self.get_dict_representation())
